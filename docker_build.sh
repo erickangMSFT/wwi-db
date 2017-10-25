2d1e41adcc105b6e54dfa755a98edacbcbd80442
Eric Kang <erickangatms@outlook.com>
2017-10-24 17:20:37 -0700

    .

--- docker_build.sh
+++ docker_build.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 docker build . --rm -t ericskang/db-jobs:migration
 
-docker rmi -f $(docker images -f "dangling=true" -q)
-
-docker login
+#docker login
 
 docker push ericskang/db-jobs:migration
-
