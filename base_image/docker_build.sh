0f8b4d7d1f9d9098de8eb205fa398ccc69dfd8ec
Eric Kang <erickangatms@outlook.com>
2017-10-24 18:39:47 -0700

    .

--- base_image/docker_build.sh
+++ base_image/docker_build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 docker build . --rm -t ericskang/db-jobs:base
 
-#docker login
-
 docker push ericskang/db-jobs:base
--- docker_build.sh
+++ docker_build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 docker build . --rm -t ericskang/db-jobs:migration
 
-#docker login
-
 docker push ericskang/db-jobs:migration
