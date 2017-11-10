f3c33b161586f0cfa3c4412c8bd15379f418514b
Eric Kang <erickangatms@outlook.com>
2017-11-10 14:10:18 -0800

    .

--- web_app/src/sql/customers_wwi.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    p.PersonID, 
-    p.FullName, 
-    p.PhoneNumber,
-    p.FaxNumber,
-    p.EmailAddress,
-    p.LogonName
-FROM Application.People p
--- web_app/src/sql/directory.sql
+++
@@ -1,3 +0,0 @@
-
-select BusinessEntityID, FirstName, LastName, EmailAddress, PhoneNumber, JobTitle, DepartmentName
-from humanresources.vemployee
