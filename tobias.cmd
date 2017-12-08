3017e28341010dad513fb9c2f2a72bb5f17f8bd5
Eric Kang <erickangatms@outlook.com>
2017-12-08 14:32:33 -0800

    .

--- tobias.cmd
+++
@@ -1,7 +0,0 @@
-REM sa password: SqlDevOps2017
-docker login sqlpass.azurecr.io -u sqlpass -p QUXKtFF1BfbwplpWN3CXEuCeLId4+Odx
-git clone https://github.com/erickangMSFT/wwi-db.git
-cd wwi-db
-docker-compose build 
-docker-compose up -d
-
